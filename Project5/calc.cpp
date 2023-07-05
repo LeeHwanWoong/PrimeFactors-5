ece19b0c4c11666f9535cf17f1cf1677ab6030fd
hwoong <lhw3110@naver.com>
2023-07-05 14:25:16 +0900

    first commit

---
+++ Project5/calc.cpp
@@ -0,0 +1,130 @@
+#include <vector>
+using namespace std;
+
+class Calc
+{
+public:
+	static int calMinus(int a, int b)
+	{
+		return ((a > b) ? (a - b) : (b - a));
+	}
+private:
+};
+
+class Fibo
+{
+public:
+	Fibo()
+	{
+		arr[0] = 0;
+		arr[1] = 1;
+		for (int i = 2; i < 100000; ++i)
+		{
+			arr[i] = -1;
+		}
+	}
+
+	int fibo(int i)
+	{
+		if(arr[i] != -1)
+		{
+			return arr[i];
+		}
+
+		arr[i] = fibo(i - 1) + fibo(i - 2);
+
+		return arr[i];
+	}
+private:
+	int arr[100000];
+};
+
+class Bank
+{
+public:
+	Bank()
+	:balance(10000)
+	{}
+	Bank(int init)
+	:balance(init)
+	{}
+
+	int getAccount()
+	{
+		return balance;
+	}
+
+	void setAccount(int money)
+	{
+		balance = money;
+	}
+
+	void withdraw(int money)
+	{
+		balance = (balance >= money) ? (balance - money) : (0);
+	}
+
+	void deposit(int money)
+	{
+		balance += money;
+	}
+
+	int profit_5Percent()
+	{
+		setProfit(5);
+		return profit_SetPercent();
+	}
+
+	void setProfit(int _percent)
+	{
+		percent = _percent;
+	}
+
+	int profit_SetPercent()
+	{
+		return balance = balance * (100 + percent) / 100;
+	}
+
+	int profit_NYear(int n)
+	{
+		int ret = balance;
+
+		for (int i = 0; i < n; ++i)
+			ret = ret * (100 + percent) / 100;
+
+		return ret;
+	}
+private:
+	int balance;
+	int percent;
+};
+
+class PrimeFactor
+{
+public:
+	vector<int> getPrimeFactor(int num)
+	{
+		vector<int> ret;
+
+		while(true)
+		{
+			for (int i = 2; i < num; ++i)
+			{
+				if(num % i == 0)
+				{
+					num /= i;
+					ret.push_back(i);
+					break;
+				}
+			}
+
+			if(num == 1)
+			{
+				break;
+			}
+		}
+
+		return ret;
+	}
+private:
+};
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,99 @@
+#include "pch.h"
+#include "../Project5/calc.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(Calc::calMinus(5,4), 1);
+  EXPECT_EQ(Calc::calMinus(5,1), 4);
+  EXPECT_EQ(Calc::calMinus(1,5), 4);
+  EXPECT_EQ(Calc::calMinus(2,3), 1);
+}
+
+TEST(TestCaseName, fibo) {
+	Fibo* f = new Fibo();
+
+	EXPECT_EQ(f->fibo(0), 0);
+	EXPECT_EQ(f->fibo(1), 1);
+	EXPECT_EQ(f->fibo(2), 1);
+	EXPECT_EQ(f->fibo(3), 2);
+	EXPECT_EQ(f->fibo(4), 3);
+	EXPECT_EQ(f->fibo(5), 5);
+	EXPECT_EQ(f->fibo(6), 8);
+	EXPECT_EQ(f->fibo(7), 13);
+	EXPECT_EQ(f->fibo(8), 21);
+	EXPECT_EQ(f->fibo(9), 34);
+}
+
+TEST(TestCaseName, bank)
+{
+	Bank b;
+
+	EXPECT_EQ(10000, b.getAccount());
+
+	b.setAccount(20000);
+	EXPECT_EQ(20000, b.getAccount());
+}
+
+TEST(TestCaseName, bankwithdraw)
+{
+	Bank b(20000);
+
+	EXPECT_EQ(20000, b.getAccount());
+
+	b.withdraw(10000);
+	EXPECT_EQ(10000, b.getAccount());
+}
+
+TEST(TestCaseName, bankdeposit)
+{
+	Bank b(30000);
+
+	EXPECT_EQ(30000, b.getAccount());
+
+	b.deposit(20000);
+	EXPECT_EQ(50000, b.getAccount());
+}
+
+TEST(TestCaseName, Profit_5percent)
+{
+	Bank b(10000);
+
+	EXPECT_EQ(10000, b.getAccount());
+
+	b.profit_5Percent();
+	EXPECT_EQ(10500, b.getAccount());
+}
+
+TEST(TestCaseName, setProfit)
+{
+	Bank b(10000);
+
+	EXPECT_EQ(10000, b.getAccount());
+
+	b.setProfit(10);
+	b.profit_SetPercent();
+	EXPECT_EQ(11000, b.getAccount());
+}
+
+TEST(TestCaseName, AfterNYear)
+{
+	Bank b(10000);
+
+	EXPECT_EQ(10000, b.getAccount());
+
+	b.setProfit(10);
+	EXPECT_EQ(12100, b.profit_NYear(2));
+	EXPECT_EQ(13310, b.profit_NYear(3));
+	EXPECT_EQ(14641, b.profit_NYear(4));
+}
+
+TEST(TestCaseName, Prime)
+{
+	PrimeFactor pf;
+	vector<int> expected;
+
+	expected.push_back(2);
+	expected.push_back(2);
+	expected.push_back(3);
+
+	EXPECT_EQ(pf.getPrimeFactor(12), expected);
+}
